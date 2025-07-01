1ac46b569bf7421c3b6c35efbb73c7fc9783a9c5
acianfro <angelo.cianfroccoii@gmail.com>
2025-07-01 16:07:29 -0400

    Update and rename WorkerProfiles.js to TestWorker.js

---
+++ src/components/workers/TestWorker.js
@@ -0,0 +1,13 @@
+// Create: src/components/workers/TestWorker.jsx
+import React from 'react';
+
+const TestWorker = () => {
+  return (
+    <div>
+      <h1>Test Worker Component</h1>
+      <p>If you see this, the routing is working!</p>
+    </div>
+  );
+};
+
+export default TestWorker;
